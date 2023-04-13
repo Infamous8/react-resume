e7d54f23ee0414b50b4d88141bd508dc152d7930
Gregory Jonathan Bales <balesg@gmail.com>
2023-04-13 14:07:57 +0200

    updates section conditional rendering logic

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -10,37 +10,37 @@ export const Home: React.FC<HomeProps> = (props) => {
 
     return (
         <PageLayout>
-            {contentSections.education && (
+            {contentSections.education.show && (
                 <ContentCell section={'education'} collapsible={true} collapsedDefault={false} >
                     Education
                 </ContentCell>
             )}
-            {contentSections.experience && (
+            {contentSections.experience.show && (
                 <ContentCell  section={'experience'} collapsible={true} collapsedDefault={false}>
                     Experience
                 </ContentCell>
             )}
-            {contentSections.skills && (
+            {contentSections.skills.show && (
                 <ContentCell  section={'skills'} collapsible={true} collapsedDefault={false}>
                     Skills
                 </ContentCell>
             )}
-            {contentSections.projects && (
+            {contentSections.projects.show && (
                 <ContentCell  section={'projects'} collapsible={true} collapsedDefault={true}>
                     Projects
                 </ContentCell>
             )}
-            {contentSections.associations && (
+            {contentSections.associations.show && (
                 <ContentCell section={'associations'} collapsible={true} collapsedDefault={true}>
                     Associations
                 </ContentCell>
             )}
-            {contentSections.interests && (
+            {contentSections.interests.show && (
                 <ContentCell  section={'interests'} collapsible={true} collapsedDefault={true}>
                     Interests
                 </ContentCell>
             )}
-            {contentSections.recognitions && (
+            {contentSections.recognitions.show && (
                 <ContentCell  section={'recognitions'} collapsible={true} collapsedDefault={true}>
                     Recognitions
                 </ContentCell>
